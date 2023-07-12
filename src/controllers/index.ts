6c681e3fff0ab1cdece1aca951938b0c58580c19
virendra13797 <virendrayogi1467@gmail.com>
2023-07-12 10:32:26 +0530

    first commit

---
+++ src/controllers/index.ts
@@ -0,0 +1,3 @@
+export * from './ping.controller';
+export * from './product.controller';
+export * from './user.controller';
---
+++ src/models/product.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Product extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  product_name: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Product>) {
+    super(data);
+  }
+}
+
+export interface ProductRelations {
+  // describe navigational properties here
+}
+
+export type ProductWithRelations = Product & ProductRelations;
